c8fd6f589925a0df0bbf5634b66c90c91d7b51dc
Bredgren <brandon.edgren@gmail.com>
2017-07-30 10:40:06 -0700

    Draw square, use geo.

--- main.go
+++ main.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 
+	"github.com/Bredgren/geo"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+var square *ebiten.Image
+
 func update(screen *ebiten.Image) error {
+	screen.Fill(color.NRGBA{0xff, 0x00, 0x00, 0xff})
 	ebitenutil.DebugPrint(screen, "Hello")
+
+	if ebiten.IsKeyPressed(ebiten.KeyF) {
+		ebitenutil.DebugPrint(screen, "\n\n\nF")
+	}
+
+	if square == nil {
+		square, _ = ebiten.NewImage(32, 32, ebiten.FilterNearest)
+	}
+
+	square.Fill(color.White)
+
+	squarePos := geo.VecXY(32, 32)
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(squarePos.XY())
+
+	screen.DrawImage(square, opts)
+
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, 320, 240, 2, "Hello World!"); err != nil {
+	if err := ebiten.Run(update, 640, 480, 1, "Hello World!"); err != nil {
 		panic(err)
 	}
 	fmt.Println("bye")
